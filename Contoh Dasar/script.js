75fc0a374d06060bba6a158ef02a8767ad24966c
Hz4E!_ <mrayhan1924@gmail.com>
2025-03-29 19:12:07 +0700

    JAVASCRIPT

---
+++ Contoh Dasar/script.js
@@ -0,0 +1,24 @@
+// Variabel
+let nama = "Budi";
+const umur = 30;
+
+// Fungsi
+function sapa(nama) {
+  console.log("Halo, " + nama + "!");
+}
+
+// Objek
+const orang = {
+  nama: "Ani",
+  umur: 25,
+  pekerjaan: "Pengembang Web"
+};
+
+// Array
+const angka = [1, 2, 3, 4, 5];
+
+// Memanggil fungsi
+sapa(nama);
+
+// Mengakses properti objek
+console.log(orang.pekerjaan);
