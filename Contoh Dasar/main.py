e3052f43bc10d38118e775b2e774f3fe0225b4b6
Hz4E!_ <mrayhan1924@gmail.com>
2025-03-29 19:18:29 +0700

    PYTHON

---
+++ Contoh Dasar/main.py
@@ -0,0 +1,20 @@
+# Komentar
+nama = "Budi" # Variabel string
+umur = 30 # Variabel integer
+
+# Fungsi
+def sapa(nama):
+  print("Halo, " + nama + "!")
+
+# Pemanggilan fungsi
+sapa(nama)
+
+# Kondisi
+if umur > 25:
+  print("Dewasa")
+else:
+  print("Muda")
+
+# Perulangan
+for i in range(5):
+  print(i)
